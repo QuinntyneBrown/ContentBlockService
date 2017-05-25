42c1974e90ff83b693880ebded7d348c7769e454
Quinntyne Brown <quinntynebrown@gmail.com>
2017-05-25 15:54:38 -0400

    clean-up

--- src/ContentBlockService/Data/Model/CallToActionContentBlock.cs
+++ src/ContentBlockService/Data/Model/CallToActionContentBlock.cs
@@ -13,13 +13,13 @@ namespace ContentBlockService.Data.Model
     {
         public int Id { get; set; }
         
-		[ForeignKey("Tenant")]
+        [ForeignKey("Tenant")]
         public int? TenantId { get; set; }
         
-		[Index("CallToActionContentBlockNameIndex", IsUnique = false)]
+        [Index("CallToActionContentBlockNameIndex", IsUnique = false)]
         [Column(TypeName = "VARCHAR")]     
-        [StringLength(MaxStringLength)]		   
-		public string Name { get; set; }
+        [StringLength(MaxStringLength)]           
+        public string Name { get; set; }
 
         public string Slug { get; set; }
 
@@ -35,13 +35,13 @@ namespace ContentBlockService.Data.Model
 
         public DateTime CreatedOn { get; set; }
         
-		public DateTime LastModifiedOn { get; set; }
+        public DateTime LastModifiedOn { get; set; }
         
-		public string CreatedBy { get; set; }
+        public string CreatedBy { get; set; }
         
-		public string LastModifiedBy { get; set; }
+        public string LastModifiedBy { get; set; }
         
-		public bool IsDeleted { get; set; }
+        public bool IsDeleted { get; set; }
 
         public virtual Tenant Tenant { get; set; }
     }
